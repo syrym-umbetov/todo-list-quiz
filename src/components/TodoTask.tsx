a0fc7f7ab1786a4438133f8707e5b0a461927071
Farabi Askar <askar.farabi12@gmail.com>
2022-05-04 20:33:29 +0600

    added status done

--- src/components/TodoTask.tsx
+++ src/components/TodoTask.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { ITask } from './../Interfaces';
+import { Checkbox } from '@mui/material'
+import './TodoTask.css'
 
 interface Props {
   task: ITask;
   completeTask(taskIdToDelete: number): void;
+  isTaskDone(taskId: number): void;
 }
 
-const TodoTask = ({ task, completeTask }: Props) => {
+const TodoTask = ({ task, completeTask, isTaskDone }: Props) => {
+
   return (
     <div className='task'>
+      <Checkbox checked={task.complete} onChange={() => isTaskDone(task.taskId)} />
       <div className='content'>
-        <span>{task.taskName}</span>
+        <span className={task.complete ? "title text" : 'title'} >{task.taskName}</span>
         <span>{task.deadline}</span>
       </div>
       <button
